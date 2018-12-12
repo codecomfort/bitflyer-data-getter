43c2b9dd1e708f615c413051a8e4037a7881a40f
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-12-13 01:41:53 +0900

    fix log duplicattion

--- logger.py
+++ logger.py
@@ -8,22 +8,31 @@ class Logger:
 
     Python の AWS Lambda 関数ログ作成 - AWS Lambda
     https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/python-logging.html
+
+    ログ出力のための print と import logging はやめてほしい - Qiita
+    https://qiita.com/amedama/items/b856b2f30c2f38665701
     """
 
     def __init__(self, name=__name__):
         self.logger = getLogger(name)
         self.logger.setLevel(DEBUG)
-        formatter = Formatter(
-            "[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] %(message)s")
+
+        # 以下の hander は使わなくてもロガー自体は動作する
+        # というか lambda とともに使用する場合は、以下のように逆に面倒が増えるかも
+
+        # formatter = Formatter(
+        #     "[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] %(message)s")
 
         # stdout
-        handler = StreamHandler()
-        handler.setLevel(DEBUG)
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        # 便利な反面、lambda 上で有効になってると、logger の分と handler の分の
+        # ２つが CloudWatch logs に出力されてしまうのでうざい
+        # handler = StreamHandler()
+        # handler.setLevel(DEBUG)
+        # handler.setFormatter(formatter)
+        # self.logger.addHandler(handler)
 
-        # lambda 上では log.log 作成権限なくてエラーになる
         # file
+        # lambda 上では log.log 作成権限なくてエラーになる
         # handler = handlers.RotatingFileHandler(filename='log.log',
         #                                        maxBytes=1048576,
         #                                        backupCount=3)
